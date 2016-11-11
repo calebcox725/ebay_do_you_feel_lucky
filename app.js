d6b37a0aa01007bd7ce770e38494623d00c87fcc
calebcox725 <caleb.cox@flatironschool.com>
2016-11-11 18:35:54 -0500

    Promise works, somehow

--- app.js
+++ app.js
@@ -3,36 +3,14 @@
     $('#submit').on('click', function() {
       event.preventDefault()
       let query = $('#search').val()
-      search(query)
-    })
-  })
+      let search = EbayAdapter.search(query)
 
-  function search(query) {
-    var timeLimit = 60000 // milliseconds
-    var maxPrice = 1
-
-    var deadline = new Date(Date.now() + timeLimit).toJSON()
-    var queryData = {
-      'OPERATION-NAME': 'findItemsAdvanced',
-      'SERVICE-VERSION': '1.0.0',
-      'SECURITY-APPNAME': 'CalebCox-Test-PRD-745f6444c-e2e81b28',
-      'RESPONSE-DATA-FORMAT': 'JSON',
-      'REST-PAYLOAD': 'true',
-      'keywords': query,
-      'paginationInput.entriesPerPage': '10',
-      'itemFilter(0).name': 'EndTimeTo',  
-      'itemFilter(0).value': deadline,
-      'itemFilter(1).name': 'ListingType',
-      'itemFilter(1).value': 'Auction',
-      'itemFilter(2).name': 'MaxPrice',
-      'itemFilter(2).value': maxPrice
-    }
-
-    $.get(`https://svcs.ebay.com/services/search/FindingService/v1`, queryData, null, 'jsonp').done(function(data) {
-      let auctions = data.findItemsAdvancedResponse[0].searchResult[0].item
-      auctions.forEach(auction => {
-        $('body').append(`<p><img src="${auction.galleryURL[0]}"><a href="${auction.viewItemURL[0]}">${auction.title[0]}</a></p>`)
+      search.then(function(auctions) {
+        auctions.forEach(auction => {
+          $('body').append(`<p><img src="${auction.galleryURL[0]}"><a href="${auction.viewItemURL[0]}">${auction.title[0]}</a></p>`)
+        })
       })
 
+
     })
-  }
+  })
