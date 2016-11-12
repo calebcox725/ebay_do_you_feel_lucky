c3c46c66e0f771b20ed9aab2efdf0ed89a1158a3
calebcox725 <caleb.cox@flatironschool.com>
2016-11-11 23:13:17 -0500

    Update adapter, app.js, and index.html

--- adapters/ebay_adapter.js
+++ adapters/ebay_adapter.js
@@ -2,9 +2,11 @@ class EbayAdapter {
   static search(query) {
     return new Promise(function(resolve) {
       var timeLimit = 60000 // milliseconds
-      var maxPrice = 1
+      var maxPrice = 1.00
 
       var deadline = new Date(Date.now() + timeLimit).toJSON()
+
+      var apiURL = `https://svcs.ebay.com/services/search/FindingService/v1`
       var queryData = {
         'OPERATION-NAME': 'findItemsAdvanced',
         'SERVICE-VERSION': '1.0.0',
@@ -12,7 +14,8 @@ class EbayAdapter {
         'RESPONSE-DATA-FORMAT': 'JSON',
         'REST-PAYLOAD': 'true',
         'keywords': query,
-        'paginationInput.entriesPerPage': '10',
+        'paginationInput.entriesPerPage': '3',
+        'sortOrder': 'BestMatch',
         'itemFilter(0).name': 'EndTimeTo',  
         'itemFilter(0).value': deadline,
         'itemFilter(1).name': 'ListingType',
@@ -21,9 +24,18 @@ class EbayAdapter {
         'itemFilter(2).value': maxPrice
       }
 
-      $.get(`https://svcs.ebay.com/services/search/FindingService/v1`, queryData, null, 'jsonp')
+      $.get(apiURL, queryData, null, 'jsonp')
         .done(function(data) {
-          resolve(data.findItemsAdvancedResponse[0].searchResult[0].item)
+          let searchResults = data.findItemsAdvancedResponse[0].searchResult[0]
+          let count = parseInt(searchResults['@count'])
+
+          if (count > 0) { 
+            var results = searchResults.item
+          } else {
+            var results = null
+          }
+
+          resolve(results)
         })
     })
   }
--- app.js
+++ app.js
@@ -6,11 +6,36 @@
       let search = EbayAdapter.search(query)
 
       search.then(function(auctions) {
-        auctions.forEach(auction => {
-          $('body').append(`<p><img src="${auction.galleryURL[0]}"><a href="${auction.viewItemURL[0]}">${auction.title[0]}</a></p>`)
-        })
+        $('#results').empty()
+
+        // auctions will be null if the search returned no matches
+        if (auctions) {
+          auctions.forEach(auction => {
+            let linkURL = auction.viewItemURL[0]
+            let imgURL = auction.galleryURL[0]
+            let description = auction.title[0]
+            let price = auction.sellingStatus[0].currentPrice[0]['__value__']
+            let location = auction.location[0].split(",").join(", ")
+
+
+            // TO DO: replace append argument with Handlebars HTML rendering
+            $('#results').append(
+              `<div class="thumbnail col-lg-4">
+                <a href="${linkURL}" target="_blank">
+                  <img src="${imgURL}" style="width: 300px; height: 300px;">
+                </a>
+                <div class="caption">
+                  <a href="${linkURL}" target="_blank">
+                    <p>${description}</p>
+                  </a>
+                    <b>$${price}</b> from ${location}
+                </div>
+              </div>`
+            )
+          })
+        } else {
+          $('#results').append('<hr><h2>No matches found!</h2>')
+        }
       })
-
-
     })
   })
