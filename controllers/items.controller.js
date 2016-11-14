984a077b8f320e82f0079e86316749206c10d121
calebcox725 <caleb.cox@flatironschool.com>
2016-11-14 10:22:46 -0500

    Merge everything, clean up whitespace, etc.

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -4,49 +4,52 @@ function search() {
   search.then(function(auctions) {
     $('#results').empty()
     if (auctions) {
-      var auction = randomize(auctions)
-      var secondsRemaining = (new Date(auction.listingInfo[0].endTime[0]) - new Date()) / 1000
-      var item = createItem(auction)
-      // TO DO: replace append argument with Handlebars HTML rendering
-      $('#results').append(
-        `<a href="${item.linkURL}" target="_blank">
-          <div class="thumbnail col-sm-4">
-            <img src="${item.imgURL}" style="max-width: 100%; min-height: 250px;">
-            <div class="caption">
-              <p>${item.description}</p>
-              <b>$${item.price}</b> from ${item.place}
+      auctions.forEach(auction => {
+        var item = createItem(auction)
+        // TO DO: replace append argument with Handlebars HTML rendering
+        $('#results').append(
+          `<a href="${item.linkURL}" target="_blank">
+            <div class="thumbnail col-sm-4">
+              <img src="${item.imgURL}" style="max-width: 100%; min-height: 250px;">
+              <div class="caption">
+                <h5>${item.description}</h5>
+                <b>$${item.price}</b> from ${item.place}
+                <div id='time${item.id}'"></div>
+              </div>
             </div>
-          </div>
-          <div id='time'></div>
-        </a>`
-      )
-      var display = $('#time')
-      startTimer(secondsRemaining, display)
+          </a>`
+        )
+
+        var timeId = $(`#time${item.id}`)
+        startTimer(item.time, timeId)
+      })
     } else {
       $('#results').append('<hr><h2>No matches found!</h2>')
     }
   })
 
-  function startTimer(duration, display) {
+  function startTimer(duration, timeId) {
       var start = Date.now(), diff, hours, minutes, seconds
       function timer() {
-          // get the number of seconds that have elapsed since
-          // startTimer() was called
           diff = duration - (((Date.now() - start) / 1000) | 0)
-          // does the same job as parseInt truncates the float
+
           hours = (diff / (3600)) | 0
           minutes = ((diff % 3600) / 60) | 0
           seconds = (diff % 60) | 0
           hours = hours < 10 ? "0" + hours : hours
           minutes = minutes < 10 ? "0" + minutes : minutes
           seconds = seconds < 10 ? "0" + seconds : seconds
-          $(display).text(hours + ":" + minutes + ":" + seconds + ")")
+
+          $(timeId).text("(" + minutes + ":" + seconds + ")").css({'font-size': '150%'})
+
+          if (seconds <= 10) {
+            $(timeId).css({ 'color': 'red'  })
+          }
+
           if(seconds <= 0){
             clearInterval(interval)
           }
       }
-      // we don't want to wait a full second before the timer starts
-
       var interval = setInterval(timer, 500)
   }
 
@@ -55,24 +58,9 @@ function search() {
     var linkURL = auction.viewItemURL[0]
     var imgURL = auction.galleryURL[0]
     var description = auction.title[0]
+    var time = (new Date(auction.listingInfo[0].endTime[0]) - new Date()) / 1000
     var price = auction.sellingStatus[0].currentPrice[0]['__value__']
     var place = auction.location[0].split(",").join(", ")
-    return new Item(linkURL, imgURL, description, price, place)
+    return new Item(linkURL, imgURL, description, time, price, place)
   }
-
-  function randomize(auction){
-    var randomAuction = auction[Math.floor(Math.random()*auction.length)]
-    return randomAuction
-  }
-
-  //   $.get(`https://svcs.ebay.com/services/search/FindingService/v1`, Data, null, 'jsonp').done(function(data) {
-  //     let auctions = data.findItemsAdvancedResponse[0].searchResult[0].item
-  //     // auctions.forEach(auction => {
-  //     //   createItem(auction)
-  //     //   $('.slick-div').append(`<p><img src="${auction.galleryURL[0]}"><a target="_blank" href="${auction.viewItemURL[0]}">${auction.title[0]}</a></p>`)
-  //     // })
-  //     // $('.slick-div').slick()
-  //   })
-  // }
-
 }
--- models/item.js
+++ models/item.js
@@ -3,11 +3,12 @@ const Item = (function(){
   let id = 0;
 
   return class {
-    constructor(linkURL, imgURL, description, price, place){
+    constructor(linkURL, imgURL, description, time, price, place){
       this.id = ++id;
       this.linkURL = linkURL;
       this.imgURL = imgURL;
       this.description = description;
+      this.time = time;
       this.price = price;
       this.place = place;
       store.data.list = [...store.data.list, this];
